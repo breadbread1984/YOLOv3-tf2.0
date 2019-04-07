2a0444577a1fda821021c7b5078f90062c00dac8
breadbread1984 <breadbread1984@163.com>
2019-04-07 20:36:58 +0800

    modified:   Label.py

--- Label.py
+++ Label.py
@@ -5,6 +5,10 @@ import tensorflow as tf;
 import tensorflow_datasets as tfds;
 
 def objects2labels(objects):
-    bbox = objects["bbox"];
-    label = objects["label"];
-    # TODO
+    
+    batch_size = objects.shape[0];
+    for b in range(batch_size):
+        for obj in objects[b]:
+            bbox = objects["bbox"]; #relative bbox = (ymin,xmin,ymax,xmax)
+            label = objects["label"];
+            # TODO
