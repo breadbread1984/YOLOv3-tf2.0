b1c4cbcae998bc082f20b535c7c1b982747b1ed4
breadbread1984 <breadbread1984@163.com>
2019-04-12 13:32:01 +0800

    modified:   preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -15,6 +15,9 @@ import tensorflow_datasets as tfds;
 # 'is_crowd': <tf.Tensor: id=539, shape=(1, 3), dtype=bool, numpy=array([[False, False, False]])>
 # }
 
+# anchor boxes are given in (width, height) order
+PRESET_ANCHORS = np.array([[10,13],[16,30],[33,23],[30,61],[62,45],[59,119],[116,90],[156,198],[373,326]], dtype = np.int32);
+
 def parse_function(serialized_example):
     
     feature = tfds.features.FeaturesDict({
@@ -28,20 +31,18 @@ def parse_function(serialized_example):
             "is_crowd": tf.bool,
         }),
     });
-    image, label = preprocess(feature["image"], feature["objects"], random = True);
+    image, bbox = preprocess(feature["image"], feature["objects"]["bbox"], random = True);
+    label = bbox_to_tensor(bbox, feature["objects"]["label"]);
     
     return image, label;
 
-def preprocess(image, objects, input_shape = (416,416), random = False, jitter = .3, hue = .1, sat = 1.5, bri = .1):
+def preprocess(image, bbox, input_shape = (416,416), random = False, jitter = .3, hue = .1, sat = 1.5, bri = .1):
     
     assert 4 == len(image.shape) and 3 == image.shape[-1];
     assert 0 < jitter < 1;
     assert -1 < hue < 1;
     assert 0 < sat;
     assert 0 < bri < 1;
-    bbox = objects["bbox"]; # bbox.shape = (batch, object_num,4), 4-tuple is (ymin,xmin,ymax,xmax)
-    label = objects["label"]; # label.shape = (batch, object_num)
-    is_crowd = objects["is_crowd"]; # is_crowd.shape = (batch, object_num)
     img_shape = image.shape[1:3]; #(height, width)
     
     if False == random:
@@ -122,3 +123,59 @@ def preprocess(image, objects, input_shape = (416,416), random = False, jitter =
         bbox = tf.expand_dims(valid_bbox, axis = 0); # bbox.shape = (1,valid box num, 4)
         # return
         return image_data, bbox;
+
+def bbox_to_tensor(bbox, label, input_shape = (416,416), anchors = PRESET_ANCHORS, num_classes = 80):
+    
+    # bbox.shape = (1, box num, 4) which represents (ymin,xmin,ymax,xmax)
+    # label.shape = (1, box num)
+    # anchors = (9,2)
+    assert tf.math.reduce_sum(tf.cast(label >= num_classes, dtype = tf.int32)).numpy() == 0;
+    num_layers = len(anchors) // 3;
+    anchor_mask = [[6,7,8],[3,4,5],[0,1,2]] if num_layers == 3 else [[3,4,5],[1,2,3]];
+    
+    true_boxes = tf.zeros_like(bbox);
+    true_boxes[..., 0:2] = tf.reverse(bbox[...,0:2] + bbox[...,2:4] // 2, axis = [-1]); # box center proportional position
+    true_boxes[..., 2:4] = tf.reverse(bbox[...,2:4] - bbox[...,0:2], axis = [-1]); # box proportional size
+    boxes_xy = true_boxes[..., 0:2] * tf.reverse(tf.convert_to_tensor(input_shape, dtype = tf.float32), axis = [0]); # box center absolute position
+    boxes_wh = true_boxes[..., 2:4] * tf.reverse(tf.convert_to_tensor(input_shape, dtype = tf.float32), axis = [0]); # box absolute size
+    
+    # create tensor for label
+    y_true = [tf.zeros(shape = (input_shape[0] // {0:32, 1:16, 2:8}[l],input_shape[1] // {0:32, 1:16, 2:8}[l],anchor_mask[l], 5 + num_classes), dtype = tf.float32) for l in range(num_layers)];
+    
+    # center the anchor boxes at the origin, get the max and min of corners' (x,y)
+    anchors = tf.expand_dims(tf.convert_to_tensor(anchors, dtype = tf.float32), 0); # anchors.shape = (1, 9, 2)
+    anchors_maxes = anchors / 2.; # max of width, height, anchors_maxes.shape = (1, 9, 2)
+    anchors_mins = -anchors_maxes; # min of width, height, anchors_mins.shape = (1, 9, 2)
+    
+    # center the bbox at the origin, get the max and min of corners' (x,y)
+    valid_mask = boxes_wh[...,0] > 0; # valid box should have width > 0: valid_mask.shape = (1, box_num)
+    wh = tf.boolean_mask(boxes_wh, valid_mask); # absolute size: wh.shape = (valid box num, 2)
+    valid_true_boxes = tf.boolean_mask(true_boxes, valid_mask); # box proportional position: valid_true_boxes.shape = (valid box num, 4)
+    valid_label = tf.boolean_mask(label, valid_mask); # valid_label.shape = (valid box num)
+    # if there is any valid bbox, get anchor box which has the maximum iou with current bbox.
+    if wh.shape[0] > 0:
+        wh = tf.expand_dims(wh, -2); # wh.shape = (valid box num, 1, 2)
+        box_maxes = wh / 2; # max of width, height, box_maxes.shape = (valid box num, 1, 2)
+        box_mins = -box_maxes; # min of width, height, box_mins.shape = (valid box num, 1, 2)
+        intersect_mins = tf.math.maximum(box_mins, anchor_mins); # intersect_mins.shape = (valid box num, anchor num(9), 2)
+        intersect_maxes = tf.math.minimum(box_maxes, anchor_maxes); # intersect_maxes.shape = (valid box num, anchor num(9), 2)
+        intersect_wh = tf.math.maximum(intersect_maxes - intersect_mins, 0.); # intersect_wh.shape = (valid box num, anchor num(9), 2)
+        intersect_area = intersect_wh[...,0] * intersect_wh[...,1]; # intersect_area.shape = (valid box num, anchor num(9))
+        box_area = wh[...,0] * wh[...,1]; # box_area.shape = (valid box_num, 1)
+        anchor_area = anchors[...,0] * anchors[...,1]; # anchor_area.shape = (1, anchor num(9))
+        iou = intersect_area / (box_area + anchor_area - intersect_area); # iou.shape = (valid box num, anchor num(9))
+        # get the anchor box having maximum iou with each true bbbox
+        best_anchor = tf.math.argmax(iou, axis = -1); # best_anchor.shape = (valid box num)
+        # fill in label tensor
+        for t, n in enumerate(best_anchor):
+            for l in range(num_layers):
+                if n in anchor_mask[l]:
+                    i = int(valid_true_boxes[t,0] * y_true[l].shape[1]) # proportional x * grid_shape.width
+                    j = int(valid_true_boxes[t,1] * y_true[l].shape[0]) # proportional y * grid_shape.height
+                    k = anchor_mask[l][n]; # best anchor box id
+                    c = valid_label[t]; # class
+                    y_true[l][0,j,i,k,0:4] = valid_true_boxes[t,0:4]; # box proportional position (w,y,width,height)
+                    y_true[l][0,j,i,k,4] = 1; # object mask
+                    y_true[l][0,j,i,k,5+c] = 1; # class mask
+
+    return y_true;
