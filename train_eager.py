159930ac5d7bc5c05ded3b5e4e9b2807b210fdf5
breadbread1984 <breadbread1984@163.com>
2019-09-30 17:13:09 +0800

    add evalution during training

--- train_eager.py
+++ train_eager.py
@@ -12,6 +12,17 @@ os.environ['TF_ENABLE_AUTO_MIXED_PRECISION'] = '1';
 #os.environ['CUDA_VISIBLE_DEVICES'] = '';
 batch_size = 8; # images of different sizes can't be stack into a batch
 
+def safe_execution(values, func):
+    assert type(values) is list;
+    try:
+        values_check = [tf.debugging.check_numerics(value, 'the value is not currect! cancel func execution!') for value in values];
+        with tf.control_dependencies(values_check):
+            func(values);
+        return True;
+    except:
+        print('invalid numeric detected!');
+        return False;
+
 def main():
 
     # yolov3 model
@@ -20,6 +31,9 @@ def main():
     # load downloaded dataset
     trainset = tfds.load(name = "coco2014", split = tfds.Split.TRAIN, download = False);
     trainset = trainset.map(map_function).repeat(100).shuffle(batch_size).batch(batch_size).prefetch(tf.data.experimental.AUTOTUNE);
+    testset = tfds.load(name = "coco2014", split = tfds.Split.TEST, download = False);
+    testset = testset.map(map_function).repeat(100).shuffle(batch_size).batch(batch_size).prefetch(tf.data.experimental.AUTOTUNE);
+    testset_iter = testset.__iter__();
     # restore from existing checkpoint
     optimizer = tf.keras.optimizers.Adam(1e-4);
     if False == os.path.exists('checkpoints'): os.mkdir('checkpoints');
@@ -29,34 +43,36 @@ def main():
     log = tf.summary.create_file_writer('checkpoints');
     # train model
     print("training...");
-    avg_loss = tf.keras.metrics.Mean(name = 'loss', dtype = tf.float32);
-    for images,labels in trainset:
+    train_loss = tf.keras.metrics.Mean(name = 'train loss', dtype = tf.float32);
+    eval_loss = tf.keras.metrics.Mean(name = 'eval_loss', dtype = tf.float32);
+    for images, labels in trainset:
         with tf.GradientTape() as tape:
             outputs = yolov3(images);
             loss = yolov3_loss([*outputs, *labels]);
         # check whether the loss numberic is correct
-        try:
-            loss_check = tf.debugging.check_numerics(loss, 'loss is not correct! will not update mean loss.');
-            with tf.control_dependencies([loss_check]):
-                avg_loss.update_state(loss);
-                print('Step #%d Loss: %.6f' % (optimizer.iterations, loss));
-        except BaseException as e:
-            print(e.message);
+        if False == safe_execution([loss], lambda x: train_loss.update_state(x[0])): continue;
+        print('Step #%d Loss: %.6f' % (optimizer.iterations, loss));
         # write log
         if tf.equal(optimizer.iterations % 10, 0):
             with log.as_default():
-                tf.summary.scalar('loss',avg_loss.result(), step = optimizer.iterations);
-            avg_loss.reset_states();
+                tf.summary.scalar('train loss',train_loss.result(), step = optimizer.iterations);
+            train_loss.reset_states();
         grads = tape.gradient(loss, yolov3.trainable_variables);
         # check whether the grad numerics is correct
-        try:
-            grads_check = [tf.debugging.check_numerics(grad, 'grads are not correct! will not apply it to the model.') for grad in grads];
-            with tf.control_dependencies(grads_check):
-                optimizer.apply_gradients(zip(grads, yolov3.trainable_variables));
-        except BaseException as e:
-            print(e.message);
+        if False == safe_execution(grads, lambda x: optimizer.apply_gradients(zip(x, yolov3.trainable_variables))): continue;
         # save model
         if tf.equal(optimizer.iterations % 1000, 0):
+            # evaluate evey 1000 steps
+            for i in range(10):
+                images, labels = next(testset_iter);
+                outputs = yolov3(images);
+                loss = yolov3_loss([*outputs, * labels]);
+                if False == safe_execution([loss], lambda x: eval_loss.update_state(x[0])): continue;
+            print('Step #%d Eval Loss: %.6f' % (optimizer.iterations, eval_loss.result()));
+            with log.as_default():
+                tf.summary.scalar('eval loss', eval_loss.result(), step = optimizer.iterations);
+            eval_loss.reset_states();
+            # save checkpoint every 1000 steps
             checkpoint.save(os.path.join('checkpoints','ckpt'));
             yolov3.save('yolov3.h5');
     yolov3.save('yolov3.h5');
