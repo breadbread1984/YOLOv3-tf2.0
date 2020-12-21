46176275d030042d09560d81f605ccddbe79f84c
breadbread1984 <breadbread1984@163.com>
2020-12-21 15:45:47 +0800

    update

--- create_dataset.py
+++ create_dataset.py
@@ -239,7 +239,7 @@ if __name__ == "__main__":
   #'''
   # this code is for testing data augmentation
   trainset_filenames = [join('trainset', filename) for filename in listdir('trainset')];
-  trainset = tf.data.TFRecordWriter(trainset_filenames).map(parse_function_generator(80));
+  trainset = tf.data.TFRecordDataset(trainset_filenames).map(parse_function_generator(80));
   for image, labels in trainset:
     image = image * 255.; # image.shape = (416, 416, 3)
     labels1, labels2, labels3 = labels; # labels1.shape = (13, 13, 3, 85) labels2.shape = (26, 26, 3, 85) labels3.shape = (52, 52, 3, 85)
