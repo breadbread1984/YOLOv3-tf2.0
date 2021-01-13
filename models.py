a5bd3d763409127158c8cfff45bd1a807a02c05d
breadbread1984 <breadbread1984@163.com>
2021-01-13 14:58:44 +0800

    update

--- models.py
+++ models.py
@@ -170,7 +170,10 @@ def Loss(img_shape, class_num = 80, ignore_thresh = 0.5):
     # 4) position loss
     # NOTE: only punish foreground area
     # NOTE: punish smaller foreground targets more harshly
-    giou_loss = tf.keras.layers.Lambda(lambda x: x[0] * tfa.losses.GIoULoss(mode = 'iou', reduction = tf.keras.losses.Reduction.NONE)(x[1], x[2]))([object_mask, true_bbox, pred_bbox]); # giou_loss.shape = (batch, grid h, grid w, anchor_num)
+    xy_loss = tf.keras.layers.Lambda(lambda x:
+      x[0] * x[1] * tf.keras.losses.BinaryCrossentropy(from_logits = False, reduction = tf.keras.losses.Reduction.NONE)(x[2], x[3])
+    )([object_mask, loss_scale, true_xy, pred_xy]); # xy_loss.shape = (batch, grid h, grid w, anchor_num)
+    wh_loss = tf.keras.layers.Lambda(lambda x: x[0] * x[1] * 0.5 * tf.math.reduce_sum(tf.math.square(x[2] - x[3]), axis = -1))([object_mask, loss_scale, true_wh, pred_wh]);
     # 5) confidence loss
     # NOTE: punish foreground area which is miss classified
     # NOTE: and punish background area which is far from foreground area and miss classified
@@ -191,7 +194,7 @@ def Loss(img_shape, class_num = 80, ignore_thresh = 0.5):
       ), axis = -1)
     )([object_mask, true_class, pred_class]); # class_loss.shape = (batch, grid h, grid w, anchor_num)
     # 7) total
-    loss = tf.keras.layers.Lambda(lambda x: tf.math.reduce_mean(tf.math.reduce_sum(tf.math.add_n(x), axis = [1,2,3]), axis = [0]))([giou_loss, confidence_loss, class_loss]); # loss.shape = ()
+    loss = tf.keras.layers.Lambda(lambda x: tf.math.reduce_mean(tf.math.reduce_sum(tf.math.add_n(x), axis = [1,2,3]), axis = [0]))([xy_loss, wh_loss, confidence_loss, class_loss]); # loss.shape = ()
     losses.append(loss);
   loss = tf.keras.layers.Lambda(lambda x: tf.math.add_n(x))(losses);
   return tf.keras.Model(inputs = (*inputs, *labels), outputs = loss);
