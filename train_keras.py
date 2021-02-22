ff29fbb8feab984e149cbcde637b905282845611
qingge.sun <qingge.sun@enflame-tech.com>
2021-02-22 17:19:56 +0800

    update

--- train_keras.py
+++ train_keras.py
@@ -8,7 +8,7 @@ from models import YOLOv3, Loss;
 from create_dataset import parse_function_generator, parse_function;
 
 environ['TF_ENABLE_AUTO_MIXED_PRECISION'] = '1';
-environ['TF_XLA_FLAGS'] = '--tf_xla_enable_xla_devices --tf_xla_auto_jit=2'
+#environ['TF_XLA_FLAGS'] = '--tf_xla_enable_xla_devices --tf_xla_auto_jit=2'
 #os.environ['TF_CPP_MIN_VLOG_LEVEL'] = '3';
 #os.environ['CUDA_VISIBLE_DEVICES'] = '';
 batch_size = 12; # images of different sizes can't be stack into a batch
@@ -16,13 +16,12 @@ batch_size = 12; # images of different sizes can't be stack into a batch
 def main():
 
   yolov3 = YOLOv3((416,416,3,), 80);
-  def LossGenerator(layer):
-    def loss(labels, outputs):
-      return Loss((416,416,3,), layer, 80)([outputs, labels]);
-    return loss;
-  yolov3.compile(optimizer = tf.keras.optimizers.Adam(1e-5), loss = {'output1': LossGenerator(0),
-                                                                     'output2': LossGenerator(1),
-                                                                     'output3': LossGenerator(2)});
+  loss1 = Loss((416,416,3,), 0, 80);
+  loss2 = Loss((416,416,3,), 1, 80);
+  loss3 = Loss((416,416,3,), 2, 80);
+  yolov3.compile(optimizer = tf.keras.optimizers.Adam(1e-5), loss = {'output1': lambda labels, outputs: loss1([outputs, labels]),
+                                                                     'output2': lambda labels, outputs: loss2([outputs, labels]),
+                                                                     'output3': lambda labels, outputs: loss3([outputs, labels])});
   # load downloaded dataset
   trainset_filenames = [join('trainset', filename) for filename in listdir('trainset')];
   testset_filenames = [join('testset', filename) for filename in listdir('testset')];
