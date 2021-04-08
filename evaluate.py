3601ad9cbe22c922a3b09d1ebac31ccb79e2cb31
breadbread1984 <breadbread1984@163.com>
2021-04-08 17:55:56 +0800

    update

--- evaluate.py
+++ evaluate.py
@@ -4,7 +4,7 @@ from absl import app, flags;
 from os.path import join;
 from pycocotools.coco import COCO;
 from pycocotools.cocoeval import COCOeval;
-import numpy;
+import numpy as np;
 import cv2;
 import tensorflow as tf;
 from Predictor import Predictor;
